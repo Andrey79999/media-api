055c71914fece65d226b6b8f0004b2c0faa64cb7
andrey <1206543@gmail.com>
2024-12-02 22:04:53 +0700

    add stream upload

--- app/routers/files.py
+++ app/routers/files.py
@@ -5,8 +5,10 @@ from database import get_db
 from models import FileMetadata
 from schemas import FileMetadataResponse
 from storage import save_file_locally, generate_uid, send_to_cloud
-from utils import extract_file_metadata
+from utils import get_file_mime_type
 from logger import logger
+from aiofiles import open as aio_open
+
 
 router = APIRouter(prefix="/files", tags=["Files"])
 
@@ -16,10 +18,22 @@ async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db
     logger.info("Start uploading file")
     try:
         uid = generate_uid()
-        file_metadata = extract_file_metadata(file)
-        local_file_path = save_file_locally(file, f"{uid}{file_metadata['extension']}")
+        extension = os.path.splitext(file.filename)[1]
+        local_file_path = f"/media/{uid}{extension.lower()}"
 
-        db_file = FileMetadata(uid=uid, **file_metadata)
+        async with aio_open(local_file_path, 'wb') as out_file:
+            while content := await file.read(1024 * 1024):
+                await out_file.write(content)
+
+        file_size = os.path.getsize(local_file_path)
+        mime_type = get_file_mime_type(file.filename)
+        db_file = FileMetadata(
+            uid=uid,
+            original_name=file.filename,
+            extension=extension.lower(),
+            size=file_size,
+            format=mime_type,
+        )
         db.add(db_file)
         db.commit()
         db.refresh(db_file)
--- app/utils.py
+++ app/utils.py
@@ -2,18 +2,6 @@ import os
 import mimetypes
 
 
-def extract_file_metadata(file) -> dict:
-    filename = file.filename
-    size = len(file.file.read())
-    extension = os.path.splitext(filename)[1]
-    return {
-        "original_name": filename,
-        "size": size,
-        "extension": extension.lower(),
-        "format": get_file_mime_type(filename)
-    }
-
-
 def get_file_mime_type(filepath):
     mime_type, _ = mimetypes.guess_type(filepath)
     return mime_type
